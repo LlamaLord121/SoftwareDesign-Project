f5c585e51e6de2b0d6e3ebcafacc75e4daaa0f7c
LlamaLord121 <colebob.jordan@gmail.com>
2026-02-03 19:00:56 -0700

    Added basic documentation for word bank header formatting

--- src/WordBankReader.java
+++ src/WordBankReader.java
@@ -2,6 +2,15 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.Random;
 
+/**
+ * Word bank is designed so thaa multiple internal banks of varying
+ * word lengths can be chosen from. This is to allow for easy expansion
+ * into a program that can pick from a variety of word lengths.
+ *
+ * Each "bank" has a header formatted as follows: -(word length)
+ * ex: -5 would preface a 5 letter length section
+ */
+
 public class WordBankReader {
     public static String getWord(int letterCount) {
         File wordBank = new File("WordBank.txt");
@@ -14,7 +23,7 @@ public class WordBankReader {
         while(scanner.hasNextLine()) {
             String currentLine = scanner.nextLine();
             
-            if (currentLine.startsWith("-")) { // Headers always start with a - (EX: -5 for a 5 letter word section)
+            if (currentLine.startsWith("-")) {
                 if(foundHeader) {
                     break;
                 } else if (currentLine.equals(lengthHeader)) {
